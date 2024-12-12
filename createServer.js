7fcc914dfc6fad591db5b3c8b39e663e401cd518
Sairaj Deshmukh <136781037+saideshmukjh1999@users.noreply.github.com>
2024-12-12 19:40:09 +0530

    Update createServer.js

--- createServer.js
+++ createServer.js
@@ -1,15 +1,15 @@
 const http = require("http");
 
-// http
-//   .createServer((req, res) => {
-//     res.write("<h1>Hello this is sairaj deshmukh</h1>");
-//     res.end();
-//   })
-//   .listen(5000);
+http
+  .createServer((req, res) => {
+    res.write("<h1>Hello this is sairaj deshmukh</h1>");
+    res.end();
+  })
+  .listen(5000);
 
-const data = function datatransfer(req, res) {
-  res.write("This, is another conscept to define create server");
-  res.end();
-};
+// const data = function datatransfer(req, res) {
+//   res.write("This, is another conscept to define create server");
+//   res.end();
+// };
 
-http.createServer(data).listen(5100);
+// http.createServer(data).listen(5100);
